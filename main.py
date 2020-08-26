b9cc5e1770b5d6d54ea64df7b08e02651cc82315
Jason Turney <jnt5211@psu.edu>
2020-08-26 18:37:03 +0000

    final3

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-cel = float(input("Enter a temperature in celsius: "))
+cel = float(input("Enter temperature in celsius: "))
 faren = (cel * 9/5) + 32
 print(cel, "° in Celsius is equivalent to ", faren, "° Fahrenheit.", sep='')
